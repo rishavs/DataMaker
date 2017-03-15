09ebe7247187072c5e22bd41ea2e92838e1be679
rishavs <rishav.sharan@live.com>
2017-03-15 14:33:13 +0530

    Used promises. working API caller

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-	console.log('error:', error); // Print the error if one occurred
-	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	console.log('body:', body); // Print the HTML for the Google homepage.
-});
 
-var rest = require('./restler');
+// posts = require('./posts.json');
+users = require('./users.json');
 
-rest.post('http://user:pass@service.com/action', {
-  	data: { id: 334 },
-	})
-	.on('complete', function(data, response) {
-  if (response.statusCode == 201) {
-    // you can get at the raw response like this...
-  }
-});
+list = users
+
+var total_count = 0
+var pass_count = 0
+var fail_count = 0
+
+var st = new Date();
+
+// Turn the list into Promises that resolve once the request returns.
+var promises = list.map(function (item) {
+    return new Promise(function (resolve, reject) {
+        //Lets configure and request
+        request({
+            url: 'http://localhost:4000/api/users',
+            method: 'POST',
+            json: { 'user' : item}
+        }, function(error, response, body) {
+            total_count = total_count + 1
+
+            if(error) {
+                console.log(error);
+                fail_count = fail_count + 1
+            } else {
+                // console.log(response.statusCode, body);
+                var dt = new Date();
+                console.log('Successfully posted in: ' + (dt - st) + " milliseconds."  );
+                pass_count = pass_count + 1
+            }
+            resolve();
+        }); // End of request
+    }); // End of Promise
+}); // End of list.map
+
+Promise.all(promises) // When all these promsies have resolved() call the .then
+    .then(function () {
+        console.log('=========================================');
+        console.log('Total : ' + total_count);
+        console.log('Pass : ' + pass_count);
+        console.log('Fail : ' + fail_count);
+        console.log('=========================================');
+        var et = new Date();
+		console.log("Total Time Taken 	= " + (et - st)/1000 + " seconds.")
+		console.log("Average Time Taken = " + (et - st)/total_count + " milliseconds.")
+    });
